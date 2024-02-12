537baf699e989a25b2b156674b1d1015965b705a
deimukasas <159789466+deimukasas@users.noreply.github.com>
2024-02-12 16:45:24 +0200

    paketiimas
    pakeitimas1

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+
+
+kintamasis = 1
+kintamasis = 2
+
+
+def finkcija():
+    print(kintamasis)
+    print(kintamasis2)
+    print(f'test:{kintamasis2}{kintamasis}')
+    print('test:{}{}'.format(kintamasis2,kintamsis))
+
+
+
+    #Komentaras 1
+
+    """
+        Komentaras 2
+
+    """
+
+    '''
+        Komentaras 3
+    '''
